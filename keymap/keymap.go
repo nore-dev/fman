600fc21187fb19494853ca3707297af5692fd3d9
noredev <noredeveloper@gmail.com>
2022-10-31 23:40:10 +0300

    feat: show help message

--- keymap/keymap.go
+++ keymap/keymap.go
@@ -26,35 +26,58 @@ type KeyMap struct {
 var Default = KeyMap{
 	MoveCursorUp: key.NewBinding(
 		key.WithKeys("w", "up", "k"),
+		key.WithHelp("w/↑/k", "Move cursor up"),
 	),
 	MoveCursorDown: key.NewBinding(
 		key.WithKeys("s", "down", "j"),
+		key.WithHelp("s/↓/j", "Move cursor down"),
 	),
 	GoToTop: key.NewBinding(
 		key.WithKeys("g"),
+		key.WithHelp("g", "Go to top"),
 	),
 	GoToBottom: key.NewBinding(
 		key.WithKeys("G"),
+		key.WithHelp("G", "Go to Bottom"),
 	),
 	GoToHomeDirectory: key.NewBinding(
 		key.WithKeys("~", "."),
+		key.WithHelp("~/.", "Go to Home Directory"),
 	),
 	GoToParentDirectory: key.NewBinding(
 		key.WithKeys("a", "h", "left"),
+		key.WithHelp("a/h/←", "Go to parent directory"),
 	),
 	GoToSelectedDirectory: key.NewBinding(
 		key.WithKeys("d", "l", "right"),
+		key.WithHelp("d/l/→", "Go to selected directory"),
 	),
 	CopyToClipboard: key.NewBinding(
 		key.WithKeys("c"),
+		key.WithHelp("c", "Copy to clipboard"),
 	),
 	OpenFile: key.NewBinding(
 		key.WithKeys("enter"),
+		key.WithHelp("enter", "Open file"),
 	),
 	ShowHiddenEntries: key.NewBinding(
 		key.WithKeys("m"),
+		key.WithHelp("m", "Show hidden entries"),
 	),
 	ToggleHelp: key.NewBinding(
 		key.WithKeys("?"),
 	),
+}
+
+func (k KeyMap) ShortHelp() []key.Binding {
+	return []key.Binding{}
+}
+
+func (k KeyMap) FullHelp() [][]key.Binding {
+	return [][]key.Binding{
+		{k.MoveCursorUp, k.MoveCursorDown},
+		{k.GoToTop, k.GoToBottom},
+		{k.GoToHomeDirectory, k.GoToParentDirectory, k.GoToSelectedDirectory},
+		{k.OpenFile, k.ShowHiddenEntries, k.CopyToClipboard},
+	}
 }
